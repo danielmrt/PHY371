eef309c1a30ba2a510092248d163fd40ee19154b
DanielMrt <daniel.martins@gordon.edu>
2014-09-04 09:06:01 -0500

    Comments

--- bill.py
+++ bill.py
@@ -7,18 +7,18 @@ Created on Wed Sep 03 20:49:36 2014
 
 print (' Hi, Welcome to the Awesome Program of Sales for MA , or APSM, made by Daniel.')
 cost = float(raw_input ("Insert the value of your bill "))
-tax = (1.0625)
-tip = (0.15)
+tax = (1.0625) #Tax + Bill
+tip = (0.15) #Just tips
 tax_total = float(cost*tax)
 tip_total = float(cost*tip)
-total = float(tax_total + tip_total)
-print ("%.2f is your total with tips. " % total)
-from datetime import datetime
+total = float(tax_total + tip_total) # 
+print ("%.2f is your total with tips. " % total) # Just using 2 numbers after the . !
+from datetime import datetime #importing the date of your computer
 now = datetime.now()
 
 print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
 
-MakeMeTrue = raw_input("What is my age? ")
+MakeMeTrue = raw_input("What is my age? ") #I am not sure about this.
 if MakeMeTrue == 21 :
     print('True')
 else :
